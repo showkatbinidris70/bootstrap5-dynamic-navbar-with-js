5e1b4743073577169646a6226c5c0b6bb54df2c9
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-06-03 10:16:52 +0600

    first

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".nav-link");
+  const currentUrl = window.location.pathname.split("/").pop();
+
+  links.forEach((link) => {
+    const linkUrl = link.getAttribute("href");
+    if (
+      linkUrl === currentUrl ||
+      (linkUrl === "index.html" && currentUrl === "")
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
